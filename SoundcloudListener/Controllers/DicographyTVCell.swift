784664e3f460509fd6036be4daf1f6aad926f6b8
Ronny Abraham <ronny.coder @gmail.com>
2017-12-31 19:00:56 +0200

    add height function for cells

--- SoundcloudListener/Controllers/DicographyTVCell.swift
+++ SoundcloudListener/Controllers/DicographyTVCell.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class DicographyTVCell: UITableViewCell {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var iview: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- SoundcloudListener/Controllers/DiscographyTableVC.swift
+++ SoundcloudListener/Controllers/DiscographyTableVC.swift
@@ -37,18 +37,21 @@ class DiscographyTableVC: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 7
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DiscographyCell", for: indexPath)
 
         // Configure the cell...
 
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0
+    }
 
     /*
     // Override to support conditional editing of the table view.
