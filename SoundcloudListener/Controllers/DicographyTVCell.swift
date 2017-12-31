635deae7e09f1e2230e06b0e7827cba392fa3233
Ronny Abraham <ronny.coder @gmail.com>
2017-12-31 19:00:18 +0200

    create custom cell class

---
+++ SoundcloudListener/Controllers/DicographyTVCell.swift
@@ -0,0 +1,24 @@
+//
+//  DicographyTVCell.swift
+//  SoundcloudListener
+//
+//  Created by ronny abraham on 12/31/17.
+//  Copyright © 2017 ronny abraham. All rights reserved.
+//
+
+import UIKit
+
+class DicographyTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
