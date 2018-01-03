e4cab67ca6a31591052771f7ead743e0dd4fd4e8
Ronny Abraham <ronny.coder @gmail.com>
2018-01-03 20:16:37 +0200

    add table and custom class to AlbumViewController

---
+++ LastFM/Controllers/AlbumViewController.swift
@@ -0,0 +1,47 @@
+//
+//  AlbumViewController.swift
+//  LastFM
+//
+//  Created by ronny abraham on 1/3/18.
+//  Copyright © 2018 ronny abraham. All rights reserved.
+//
+
+import UIKit
+
+class AlbumViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TrackCell", for: indexPath)
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
